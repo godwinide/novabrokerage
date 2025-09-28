f7487f498cfdf10d00cc06b230aa9e524cfc5db6
Null Dev <godwinide@gmail.com>
2025-09-28 14:38:58 +0100

    first commit

---
+++ model/Site.js
@@ -0,0 +1,22 @@
+const { model, Schema } = require("mongoose");
+
+const SiteSchema = new Schema({
+    bitcoinAddress: {
+        type: String,
+        required: false
+    },
+    bchAddress: {
+        type: String,
+        required: false
+    },
+    ethereumAddress: {
+        type: String,
+        required: false
+    },
+    usdtAddress: {
+        type: String,
+        required: false
+    }
+});
+
+module.exports = Site = model("Site", SiteSchema);
